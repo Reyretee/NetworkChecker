3d6d869f96353ef67af39465ec7b6ac5a71c9438
Reyretee <ahmet07c09@gmail.com>
2024-09-04 15:58:50 +0300

    Initial commit

---
+++ src/main/java/me/reyretee/networkChecker/NetworkChecker.java
@@ -0,0 +1,17 @@
+package me.reyretee.networkChecker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NetworkChecker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
