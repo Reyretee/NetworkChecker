a4da574024f02df87620b4616f5c7f22a83de909
Reyretee <ahmet07c09@gmail.com>
2024-09-05 11:33:13 +0300

    added messages & reload command

--- src/main/java/me/reyretee/networkChecker/NetworkChecker.java
+++ src/main/java/me/reyretee/networkChecker/NetworkChecker.java
@@ -2,6 +2,8 @@ package me.reyretee.networkChecker;
 
 import me.reyretee.networkChecker.network.SocketClient;
 import me.reyretee.networkChecker.task.PingTask;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Timer;
@@ -15,7 +17,33 @@ public final class NetworkChecker extends JavaPlugin {
     @Override
     public void onEnable() {
         saveDefaultConfig();
+        saveDefaultMessages();
+        loadMessages();
+
+        setupTask();
+    }
+
+    @Override
+    public void onDisable() {
+        if (timer != null) {
+            timer.cancel();
+        }
+
+        getLogger().info("NetworkChecker kapatıldı.");
+    }
+
+    private void saveDefaultMessages() {
+        if (!getConfig().contains("messages")) {
+            saveResource("messages.yml", false);
+        }
+    }
+
+    private void loadMessages() {
         reloadConfig();
+    }
+
+    private void setupTask() {
+        reloadConfig();  // Config'i yeniden yükle
 
         String ip = getConfig().getString("bot.ip");
         int port = getConfig().getInt("bot.port");
@@ -35,18 +63,44 @@ public final class NetworkChecker extends JavaPlugin {
 
         socketClient = new SocketClient(ip, port, logger);
 
-        timer = new Timer();
-        timer.schedule(new PingTask(this, socketClient), 0, interval * 1000L);
-
-        logger.info("NetworkChecker başlatıldı. Ping verileri " + interval + " saniye aralıkla gönderilecek.");
-    }
-
-    @Override
-    public void onDisable() {
         if (timer != null) {
             timer.cancel();
         }
 
-        getLogger().info("NetworkChecker kapatıldı.");
+        timer = new Timer();
+        timer.schedule(new PingTask(this, socketClient), 0, interval * 1000L);
+
+        logger.info("NetworkChecker başlatıldı ve ping görevleri " + interval + " saniye aralıklarla çalışacak.");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("networkchecker")) {
+            if (args.length == 1) {
+                String subCommand = args[0].toLowerCase();
+
+                switch (subCommand) {
+                    case "reload":
+                        try {
+                            reloadConfig();
+                            loadMessages();
+                            setupTask();
+                            String successMessage = getConfig().getString("messages.reload_success", "Ayarlar başarıyla yeniden yüklendi!");
+                            sender.sendMessage(successMessage.replace("&", "§"));
+                        } catch (Exception e) {
+                            String errorMessage = getConfig().getString("messages.reload_fail", "Ayarlar yeniden yüklenirken bir hata oluştu!");
+                            sender.sendMessage(errorMessage.replace("&", "§"));
+                        }
+                        return true;
+                    default:
+                        sender.sendMessage("Geçersiz komut. Kullanılabilir komut: /networkchecker reload");
+                        return false;
+                }
+            } else {
+                sender.sendMessage("Geçersiz komut. Kullanılabilir komut: /networkchecker reload");
+                return false;
+            }
+        }
+        return false;
     }
 }
